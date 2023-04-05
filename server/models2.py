ac4d7076fa7cb2cbf05033a9640c0485238f8463
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-04-05 09:37:04 -0600

    complete

--- server/models2.py
+++ server/models2.py
@@ -21,7 +21,7 @@ class Game(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    reviews = db.relationship('Review', backref='game')    
+    reviews = db.relationship('Review', backref='game')
 
     def __repr__(self):
         return f'<Game {self.title} for {self.platform}>'
@@ -30,7 +30,7 @@ class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
 
     serialize_rules = ('-game.reviews', '-user.reviews',)
-
+    
     id = db.Column(db.Integer, primary_key=True)
     score = db.Column(db.Integer)
     comment = db.Column(db.String)
@@ -38,20 +38,20 @@ class Review(db.Model, SerializerMixin):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
         return f'<Review ({self.id}) of {self.game}: {self.score}/10>'
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
-    
+
     serialize_rules = ('-reviews.user',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())    
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     reviews = db.relationship('Review', backref='user')
